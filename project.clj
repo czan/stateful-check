2312e5b789950977217f1fb9a60b6b016c37b6f3
Carlo Zancanaro <carlo@zancanaro.id.au>
2015-07-29 11:20:08 +1000

    Update to test.check 0.7.0

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/test.check "0.5.9"]])
+                 [org.clojure/test.check "0.7.0"]])
