bd9dd38638501795fa1cab08feaad15c93de340d
Carlo Zancanaro <carlo@zancanaro.id.au>
2015-07-29 12:29:28 +1000

    Fix warnings from lein check/kibit/yagni

--- src/stateful_check/gen.clj
+++ src/stateful_check/gen.clj
@@ -3,12 +3,12 @@
 
 (defn is-let? [forms]
   (and (keyword? (first forms))
-       (= (namespace (first forms)) nil)
+       (nil? (namespace (first forms)))
        (= (name (first forms)) "let")))
 
 (defn is-bind? [forms]
   (and (symbol? (second forms))
-       (= (namespace (second forms)) nil)
+       (nil? (namespace (second forms)))
        (= (name (second forms)) "<-")))
 
 (defn gdo* [forms bind]
@@ -21,9 +21,6 @@
    :else `(~bind ~(first forms)
                     (fn [_#]
                       ~(gdo* (next forms) bind)))))
-
-(defmacro gdo [& forms]
-  (gdo* forms `gen/bind))
 
 (defmacro gen-do [& forms]
   (gdo* forms `gen/gen-bind))
