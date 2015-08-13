3fe8dd701471d941971e9c81f9f35b1d4eae8f28
Carlo Zancanaro <carlo@zancanaro.id.au>
2015-08-13 15:51:56 +1000

    Simplify the new shrinking stuff; changelog
    The chunked approach is still way too slow (visits too many nodes), so
    we're back to using a much simpler implementation that's better. I think
    I'm finally starting to understand why that might be the case, so
    hopefully I'll stop trying to do the chunked approach now.
    
    I also got around to adding stuff to the changelog.

--- src/stateful_check/core_utils.clj
+++ src/stateful_check/core_utils.clj
@@ -68,45 +68,13 @@
                                (generate-commands* spec state size count)))
                      (generate-commands* spec state size count)))]])))
 
-(defn remove-chunks
-  "Try removing 'chunks' of adjacent elements for shrinking purposes."
-  [roses]
-  (for [size (range (count roses) 0 -1)
-        i (range 0 (count roses) size)]
-    (into (vec (take i roses))
-          (drop (+ i size) roses))))
-
-(defn remove-pairs
-  "Try removing pairs of elements (not necessarily adjacent) for
-  shrinking purposes."
-  [roses]
-  (for [i (range 0 (dec (count roses)))
-        :let [initial (vec (take i roses))]
-        j (range (inc i) (count roses))
-        :let [middle (->> roses
-                          (take j)
-                          (drop (inc i)))
-              tail (drop (inc j) roses)]]
-    (-> initial
-        (into middle)
-        (into tail))))
-
 (defn shrink-roses
   "Shrink the command rose trees (by removing commands and shrinking
   command arguments)."
   [roses]
-  (->> (let [chunks (remove-chunks roses)
-             pairs (remove-pairs roses)]
-         (concat chunks
-                 pairs
-                 (rose/permutations roses)
-                 ;; these are here because sometimes an accurate
-                 ;; shrink involves removing a chunk/pair *and*
-                 ;; shrinking a command's arguments at the same time
-                 ;; (which is what these do)
-                 (mapcat rose/permutations chunks)
-                 (mapcat rose/permutations pairs)))
-       (remove empty?)))
+  (let [singles (rose/remove roses)
+        pairs (mapcat (comp rose/remove vec) singles)]
+    (concat singles pairs)))
 
 (defn concat-command-roses
   "Take a seq of rose trees and concatenate them. Create a vector from
