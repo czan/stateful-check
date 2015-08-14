6ebe404dc109688b152d940cb2ed2c10d7c7d2df
Carlo Zancanaro <carlo@zancanaro.id.au>
2015-08-14 15:41:43 +1000

    Update version to 0.3.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.czan/stateful-check "0.3.1"
+(defproject org.clojars.czan/stateful-check "0.3.2-SNAPSHOT"
   :description "Stateful generative testing in clojure"
   :url "https://github.com/czan/stateful-check"
   :license {:name "Eclipse Public License"
