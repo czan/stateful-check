b62d240642425941682aa899f2436bb8d445309d
Michael Drogalis <mjd3089@rit.edu>
2016-01-08 18:02:27 -0800

    Change the path so I can deploy my own copy.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.czan/stateful-check "0.3.1"
+(defproject mdrogalis/stateful-check "0.3.2"
   :description "Stateful generative testing in clojure"
   :url "https://github.com/czan/stateful-check"
   :license {:name "Eclipse Public License"
