aa347af49165ad1f051e2172cc5c15e60a95c645
Michael Drogalis <mjd3089@rit.edu>
2016-01-08 18:00:26 -0800

    Upgrades to test.check 0.9.0.

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/czan/stateful-check"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/test.check "0.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/test.check "0.9.0"]])
