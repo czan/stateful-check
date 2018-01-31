7fc036cd3fb83db68505ae558c4e1b039d62a236
Carlo Zancanaro <carlo@zancanaro.id.au>
2018-02-01 10:37:40 +1100

    Cleanup the Java map test a bit

--- test/stateful_check/java_map_test.clj
+++ test/stateful_check/java_map_test.clj
@@ -6,53 +6,37 @@
 (def test-keys ["a" "b"])
 ;; ["" "a" "house" "tree" "λ"]
 
-(def new-command
-  {:model/requires (fn [states]
-                     (empty? states))
-   :real/command (fn [] (java.util.TreeMap.))
-   :next-state (fn [states _ result]
-                 (conj states [result nil]))})
-
 (def put-command
-  {:model/requires (fn [states] (seq states))
-   :model/args (fn [states]
-                 [(gen/elements (map first states))
+  {:model/args (fn [state]
+                 [(:map state)
                   (gen/elements test-keys)
                   gen/int])
    :real/command (fn [^java.util.Map map key val]
                    (.put map key val))
-   :next-state (fn [states [m k v] _]
-                 (map (fn [[map contents]]
-                        (if (identical? m map)
-                          [map (assoc contents k v)]
-                          [map contents]))
-                      states))})
+   :next-state (fn [state [m k v] _]
+                 (update state :contents assoc k v))})
 
 (def get-command
-  {:model/requires (fn [states] (and (seq states)
-                                    (seq (keys (apply merge (map second states))))))
-   :model/args (fn [states]
-                 [(gen/elements (map first states))
+  {:model/requires (fn [state]
+                     (seq (:contents state)))
+   :model/args (fn [state]
+                 [(:map state)
                   (gen/elements test-keys)])
    :real/command (fn [^java.util.Map map key]
                    (.get map key))
    :real/postcondition (fn [prev-state _ [m k] val]
-                         (= (get (some (fn [[map contents]]
-                                         (when (identical? m map)
-                                           contents))
-                                       prev-state)
-                                 k)
+                         (= (get-in prev-state [:contents k])
                             val))})
 
 (def java-map-specification
-  {:commands {:new #'new-command
-              :put #'put-command
-              :get #'get-command}})
+  {:commands {:put #'put-command
+              :get #'get-command}
+   :initial-state (fn [setup] {:map setup, :contents {}})
+   :real/setup #(java.util.TreeMap.)})
 
 (deftest ^:slow java-map-fails-concurrently
   (is (specification-correct? java-map-specification))
   (is (not (specification-correct? java-map-specification
                                    {:gen {:threads 2
                                           :max-length 5}
-                                    :run {:max-tries 50}
-                                    :report {:first-case? true}}))))
+                                    :run {:max-tries 50}}))))
