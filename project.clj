fa485c520388e22a715950dba93b36d4472b58ed
Carlo Zancanaro <carlo@zancanaro.id.au>
2018-02-13 01:38:11 +1100

    Change the license to the MIT License

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject org.clojars.czan/stateful-check "0.4.0-SNAPSHOT"
   :description "Stateful generative testing in clojure"
   :url "https://github.com/czan/stateful-check"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/test.check "0.9.0"]]
   :test-selectors {:default (complement :slow)
