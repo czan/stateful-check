cfa6220500c62f17abb3f75c0ca19808fcadef91
Roman Scherer <roman@burningswell.com>
2023-06-06 22:32:55 +0200

    Update test.check to 1.1.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/test.check "0.9.0"]]
+                 [org.clojure/test.check "1.1.0"]]
   :test-selectors {:default #(not (:interactive %))
                    :interactive :interactive}
   :repositories [["releases" {:url "https://clojars.org/repo"
